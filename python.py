f7c77593649b4fe87bbcc21d764f19764ba444bb
Vigneswaran V <vigneswaran7373@gmail.com>
2024-08-23 11:30:06 +0530

    Initial commit

---
+++ python.py
@@ -0,0 +1,6 @@
+a=int(input("enter the 1st number:"))
+b=int(input("enter the 2st number:"))
+c=int(input("enter the 3st number:"))
+d=int(input("enter the 4st number:"))
+e=a+b+c+d/4
+print("the output is:",e)
---
+++ test.py
@@ -0,0 +1,4 @@
+a=int(input("enter the 1st number:"))
+b=int(input("enter the 2st number:"))
+c=a+b
+print("the output is:",c)
